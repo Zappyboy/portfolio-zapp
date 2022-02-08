355338ee2e0013af75240ac3ae68eaba3930aa4a
ZappF <helloworld.zapp@gmail.com>
2022-02-08 16:11:21 +0100

    Hero page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Portfolio
+      </SectionTitle>
+      <SectionText>
+        Mijn Portfolio Presentatie
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
